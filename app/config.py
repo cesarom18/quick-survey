74abed065b10a209d07442c6f92ae701daa4f71b
cesarom18 <cesarosses885@gmail.com>
2026-03-03 11:44:25 -0300

    Create project config file for backend

---
+++ app/config.py
@@ -0,0 +1,17 @@
+# app/config.py
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache # This method will remember the last return value if the arguments were the same
+
+class Settings(BaseSettings):  # Class will attempt to determine values from .env file
+    # DB info
+    db_user: str = "root"
+    db_password: str = "example"
+    db_host: str = "localhost"
+    db_name: str = "example"
+    # DB connection settings
+    echo: bool = False
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+@lru_cache()
+def get_settings() -> Settings: # This function will return app settings
+    return Settings()
