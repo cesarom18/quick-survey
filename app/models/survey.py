beb0208e20c6b5162c5ef69b61afc1c39a7b35a4
cesarom18 <cesarosses885@gmail.com>
2026-03-08 17:43:41 -0300

    Define relationship between survey and survey_category models

--- app/models/survey.py
+++ app/models/survey.py
@@ -1,8 +1,13 @@
-from sqlmodel import SQLModel, Field, table
+from sqlmodel import SQLModel, Field, Relationship
 from sqlalchemy import Column, Text
+
+from models.survey_category import SurveyCategory
 
 class Survey(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title: str = Field(max_length=150)
     description: str | None = Field(default=None, max_length=255)
     image: str = Field(sa_column=Column(Text))
+    # SurveyCategory Relationship
+    survey_category_id: int = Field(foreign_key="survey_category.id")
+    survey_category: SurveyCategory = Relationship(back_populates="surveys")
--- app/models/survey_category.py
+++ app/models/survey_category.py
@@ -1,5 +1,7 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 
 class SurveyCategory(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(max_length=255)
+    # Survey Relationship
+    surveys: list["Survey"] = Relationship(back_populates="survey_category")
