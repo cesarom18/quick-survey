5b92c8b2af115c3bf1433397b9b7500515386f93
cesarom18 <cesarosses885@gmail.com>
2026-03-08 20:41:43 -0300

    Define relationship between question and question type models

--- app/models/question.py
+++ app/models/question.py
@@ -1,6 +1,11 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+
+from models.question_type import QuestionType
 
 class Question(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     text: str = Field(max_length=255)
     order_index: int
+    # Question Type Relationship
+    question_type_id: int = Field(foreign_key="question_type.id")
+    question_type: QuestionType = Relationship(back_populates="questions")
--- app/models/question_type.py
+++ app/models/question_type.py
@@ -1,5 +1,7 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 
 class QuestionType(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(max_length=255)
+    # Question Relationship
+    questions: list["Question"] = Relationship(back_populates="question_type")
