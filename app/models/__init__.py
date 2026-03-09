d637c2ea83010f774b215928b68776457b7c3844
cesarom18 <cesarosses885@gmail.com>
2026-03-09 19:55:55 -0300

    Remove imports from __init__ models module

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,1 @@
-from .answer import Answer
-from .question import Question
-from .question_option import QuestionOption
-from .question_type import QuestionType
-from .survey import Survey
-from .survey_category import SurveyCategory
-from .survey_response import SurveyResponse
-from .user import User
+
