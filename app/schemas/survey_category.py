d1672daaa1640385e03a916593cf71a1143f536b
cesarom18 <cesarosses885@gmail.com>
2026-03-13 20:24:35 -0300

    Add survey category schema

---
+++ app/schemas/survey_category.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class BaseSurveyCategory(BaseModel):
+    model_config = ConfigDict(str_strip_whitespace=True, extra="forbid")
+
+class GetSurveyCategory(BaseModel):
+    id: int
+    name: str
+
+class CreateSurveyCategory(BaseSurveyCategory):
+    name: str = Field(max_length=255)
