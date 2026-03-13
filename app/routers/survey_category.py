32ef6c45dd97b85e8b46f2c42de4e95fdd6d1ab4
cesarom18 <cesarosses885@gmail.com>
2026-03-13 20:56:45 -0300

    Add endpoint to create survey category

--- app/routers/survey_category.py
+++ app/routers/survey_category.py
@@ -3,6 +3,7 @@ from sqlmodel import select
 
 from app.config.database import SessionDep
 from app.models.survey_category import SurveyCategory
+from app.schemas.survey_category import GetSurveyCategory, CreateSurveyCategory
 
 router = APIRouter(prefix="/survey-category", tags=["SurveyCategory"])
 
@@ -11,16 +12,16 @@ router = APIRouter(prefix="/survey-category", tags=["SurveyCategory"])
     "/",
     summary="List survey categories",
     description="Get all survey categories paginated, also you can search by name",
-    response_model=list[SurveyCategory],
+    response_model=list[GetSurveyCategory],
 )
-async def get_all(session: SessionDep):
+async def get_survey_categories(session: SessionDep):
     try:
         result = await session.execute(select(SurveyCategory))
         categories = result.scalars().all()
         return categories
-    except Exception as ex:
+    except Exception:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=ex
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error"
         )
 
 
@@ -28,21 +29,41 @@ async def get_all(session: SessionDep):
     "/{category_id}",
     summary="Get survey category",
     description="Get survey category by id",
-    response_model=SurveyCategory,
+    response_model=GetSurveyCategory,
 )
-async def get_by_id(session: SessionDep, category_id: int):
+async def get_survey_category(session: SessionDep, category_id: int):
     try:
         result = await session.execute(
             select(SurveyCategory).where(SurveyCategory.id == category_id)
         )
         category = result.scalar_one_or_none()
+        # Check if category exists
         if not category:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Survey category not found",
             )
         return category
-    except Exception as ex:
+    except Exception:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=ex
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error"
+        )
+
+
+@router.post(
+    "/",
+    summary="Post survey category",
+    description="Create survey category",
+    response_model=GetSurveyCategory,
+)
+async def create_survey_category(session: SessionDep, data: CreateSurveyCategory):
+    try:
+        category = SurveyCategory(**data.model_dump())
+        session.add(category)
+        await session.commit()
+        await session.refresh(category)
+        return category 
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error"
         )
