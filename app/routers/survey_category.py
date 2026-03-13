3c9697d2c43b026a1fe56d394d66d5f4633aac98
cesarom18 <cesarosses885@gmail.com>
2026-03-13 17:05:36 -0300

    Create endpoint to get all survey categories

--- app/routers/survey_category.py
+++ app/routers/survey_category.py
@@ -1,3 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from sqlmodel import select
+
+from app.config.database import SessionDep
+from app.models.survey_category import SurveyCategory
 
 router = APIRouter(prefix="/survey-category")
+
+
+@router.get(
+    "/",
+    summary="List survey categories",
+    description="Get all survey categories paginated, also you can search by name",
+    response_model=list[SurveyCategory],
+)
+async def get_all(session: SessionDep):
+    try:
+        result = await session.execute(select(SurveyCategory))
+        categories = result.scalars().all()
+        return categories
+    except Exception as ex:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=ex
+        )
+
