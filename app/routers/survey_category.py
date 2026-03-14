09a6194745de0c8de44e414518c2e51993474a85
cesarom18 <cesarosses885@gmail.com>
2026-03-14 16:55:58 -0300

    Add endpoint to update survey category

--- app/routers/survey_category.py
+++ app/routers/survey_category.py
@@ -75,17 +75,50 @@ async def create_survey_category(session: SessionDep, data: CreateSurveyCategory
         )
 
 
+@router.put(
+    "/{category_id}",
+    summary="Put survey category",
+    description="Update survey category",
+    status_code=status.HTTP_200_OK,
+    response_model=GetSurveyCategory,
+)
+async def update_survey_category(
+    session: SessionDep,
+    category_id: Annotated[int, Path(gt=0)],
+    data: CreateSurveyCategory,
+):
+    try:
+        category = await session.get(SurveyCategory, category_id)
+        if not category:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Survey category not found",
+            )
+        updated_data = data.model_dump(exclude_unset=True)
+        for key, value in updated_data.items():
+            setattr(category, key, value)
+        session.add(category)
+        await session.commit()
+        await session.refresh(category)
+        return category
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error",
+        )
+
+
 @router.delete(
     "/{category_id}",
     summary="Delete survey category",
     description="Delete survey category",
-    status_code=status.HTTP_204_NO_CONTENT
+    status_code=status.HTTP_204_NO_CONTENT,
 )
 async def delete_survey_category(
     session: SessionDep, category_id: Annotated[int, Path(gt=0)]
 ):
     try:
-        category = await session.get(SurveyCategory, category_id) 
+        category = await session.get(SurveyCategory, category_id)
         if not category:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
