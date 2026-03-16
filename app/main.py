4b8cc59de96724f40df4c7bc1f91a35d0a983f2f
cesarom18 <cesarosses885@gmail.com>
2026-03-16 13:42:56 -0300

    Add user router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from app.routers import survey_category
+from app.routers import survey_category, user
 from app.exceptions.handlers import register_exception_handlers
 
 app = FastAPI() # Create fastapi instance
 register_exception_handlers(app)
 # Add routers
 app.include_router(survey_category.router, prefix="/api")
+app.include_router(user.router, prefix="/api")
