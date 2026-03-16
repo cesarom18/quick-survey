60b4fb5b7f19d3781b8cf93a362ed62d0ac890ec
cesarom18 <cesarosses885@gmail.com>
2026-03-16 14:10:16 -0300

    Add endpoint to get all users

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,3 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from sqlmodel import select
+
+from app.config.database import SessionDep
+from app.models.user import User
+from app.schemas.user import GetUser
 
 router = APIRouter(prefix="/users", tags=["User"])
+
+@router.get("/", summary="Get users", description="Get users", status_code=status.HTTP_200_OK, response_model=list[GetUser])
+async def get_users(session: SessionDep):
+    result = await session.execute(select(User))
+    users = result.scalars().all()
+    return users
