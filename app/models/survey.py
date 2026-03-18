0d9978ba1ffde9e7c4f4d5fe3218e112b25be123
cesarom18 <98192523+cesarom18@users.noreply.github.com>
2026-03-18 19:47:19 -0300

    Add created at field in user and survey models

--- app/models/survey.py
+++ app/models/survey.py
@@ -1,14 +1,17 @@
 from sqlmodel import SQLModel, Field, Relationship
 from sqlalchemy import Column, Text
+from datetime import datetime, timezone
 
 from app.models.survey_category import SurveyCategory
 from app.models.user import User
+
 
 class Survey(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title: str = Field(max_length=150)
     description: str | None = Field(default=None, max_length=255)
     image: str = Field(sa_column=Column(Text))
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     # SurveyCategory Relationship
     survey_category_id: int = Field(foreign_key="survey_category.id")
     survey_category: SurveyCategory = Relationship(back_populates="surveys")
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field, Relationship
 from sqlalchemy import Column, Text
+from datetime import datetime, timezone
 
 
 class User(SQLModel, table=True):
@@ -7,6 +8,7 @@ class User(SQLModel, table=True):
     name: str = Field(max_length=100)
     email: str = Field(unique=True)
     password: str = Field(min_length=8, sa_column=Column(Text))
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     # Survey Relationship
     surveys: list["Survey"] = Relationship(back_populates="user")
     # Survey Response Relationship
