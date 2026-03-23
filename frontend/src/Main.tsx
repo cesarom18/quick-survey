031ebe44952698a6d04bb26df8353e9ede8b1ea8
cesarom18 <cesarosses885@gmail.com>
2026-03-23 15:04:01 -0300

    Add app router

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
 
-import "./assets/css/styles.css"
+import "./assets/css/styles.css";
+import { router } from "./router.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <div>asd</div>
+        <RouterProvider router={router} />
     </StrictMode>
 )
