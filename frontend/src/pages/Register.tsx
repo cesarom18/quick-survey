29d3e1bb9bd678b3c64e738ec58a52263033f9eb
cesarom18 <cesarosses885@gmail.com>
2026-03-25 19:24:42 -0300

    Add register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,14 +1,20 @@
-import { Button, Card, Form, Link, Input, Label, TextField } from "@heroui/react";
+import { Button, Card, Form, Input, Label, Link, TextField } from "@heroui/react";
 
-export const Login = () => {
+export const Register = () => {
     return (
         <Card className="rounded-3xl gap-4 w-md p-10">
             <Card.Header className="text-center">
                 <Card.Title className="text-xl font-extrabold mb-2">Welcome to Quick Survey</Card.Title>
-                <Card.Description className="text-sm">Enter your credentials toa access your profile</Card.Description>
+                <Card.Description className="text-sm">Enter then following fields and create surveys!</Card.Description>
             </Card.Header>
             <Card.Content>
                 <Form className="flex flex-col gap-4">
+                    <TextField type="text" name="name">
+                        <Label>Name</Label>
+                        <Input
+                            variant="secondary"
+                            placeholder="John" />
+                    </TextField>
                     <TextField type="email" name="email">
                         <Label>Email</Label>
                         <Input
@@ -16,24 +22,26 @@ export const Login = () => {
                             placeholder="john@example.com" />
                     </TextField>
                     <TextField type="password" name="password">
-                        <div className="flex justify-between">
-                            <Label>Password</Label>
-                            <Link className="no-underline hover:underline">
-                                Forgot?
-                            </Link>
-                        </div>
+                        <Label>Password</Label>
                         <Input
                             variant="secondary"
                             placeholder="Enter your password" />
                     </TextField>
-                    <Button className="text-white rounded-xl" size="lg" fullWidth>Sign In</Button>
+                    <TextField type="password" name="confirm-password">
+                        <Label>Confirm Password</Label>
+                        <Input
+                            variant="secondary"
+                            placeholder="Enter your password" />
+                    </TextField>
+                    <Button className="text-white rounded-xl" size="lg" fullWidth>Sign Up</Button>
                 </Form>
             </Card.Content>
             <Card.Footer className="justify-center">
                 <Link className="no-underline hover:underline">
-                    Don't have an account?
+                    Have an account?
                 </Link>
             </Card.Footer>
         </Card>
     )
 }
+
