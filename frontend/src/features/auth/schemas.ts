772fa17f62b461eedaac40e98240fea4b5610dca
cesarom18 <cesarosses885@gmail.com>
2026-04-07 21:02:03 -0400

    Add loginUser schema in Login page

--- frontend/src/features/auth/schemas.ts
+++ frontend/src/features/auth/schemas.ts
@@ -1,20 +1,28 @@
 import { z } from "zod";
 
+const emailValidation = z.email("Invalid email");
+const passwordValidation = z
+	.string()
+	.min(8, "Pasword must be more than 8 characters")
+	.max(64, "Password must be less than 64 characters")
+	.regex(/^[a-zA-Z0-9]+$/, "Password must be alphanumeric");
+
 export const registerUser = z
 	.object({
 		name: z
 			.string()
 			.min(2, "Name must be more than 2 characters")
 			.max(100, "Name must be less than 100 characters"),
-		email: z.email("Invalid email"),
-		password: z
-			.string()
-			.min(8, "Pasword must be more than 8 characters")
-			.max(64, "Password must be less than 64 characters")
-			.regex(/^[a-zA-Z0-9]+$/, "Password must be alphanumeric"),
+		email: emailValidation,
+		password: passwordValidation,
 		confirm_password: z.string(),
 	})
 	.refine((data) => data.password === data.confirm_password, {
 		message: "Passwords don't match",
 		path: ["confirm_password"],
 	});
+
+export const loginUser = z.object({
+	email: emailValidation,
+	password: passwordValidation,
+});
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,20 +1,60 @@
-import { Button, Form, Input, Label, Link, TextField } from "@heroui/react";
+import {
+	Button,
+	FieldError,
+	Form,
+	Input,
+	Label,
+	Link,
+	TextField,
+} from "@heroui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { z } from "zod";
+
+import { loginUser } from "../features/auth/schemas";
+
+type FormValues = z.infer<typeof loginUser>;
 
 export const Login = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormValues>({ resolver: zodResolver(loginUser) });
+
+	const onSubmit = (data: FormValues) => {
+		console.log(data);
+	};
+
 	return (
-		<Form className="flex flex-col gap-4">
-			<TextField type="email" name="email">
+		<Form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+			<TextField type="email" name="email" isInvalid={!!errors.email}>
 				<Label>Email</Label>
-				<Input variant="secondary" placeholder="john@example.com" />
+				<Input
+					variant="secondary"
+					placeholder="john@example.com"
+					{...register("email")}
+				/>
+				{errors?.email && <FieldError>{errors.email.message}</FieldError>}
 			</TextField>
-			<TextField type="password" name="password">
+			<TextField type="password" name="password" isInvalid={!!errors.password}>
 				<div className="flex justify-between">
 					<Label>Password</Label>
 					<Link className="no-underline hover:underline">Forgot?</Link>
 				</div>
-				<Input variant="secondary" placeholder="Enter your password" />
+				<Input
+					variant="secondary"
+					placeholder="Enter your password"
+					{...register("password")}
+				/>
+				{errors?.password && <FieldError>{errors.password.message}</FieldError>}
 			</TextField>
-			<Button className="text-white rounded-xl" size="lg" fullWidth>
+			<Button
+				className="text-white rounded-xl"
+				type="submit"
+				size="lg"
+				fullWidth
+			>
 				Sign In
 			</Button>
 		</Form>
