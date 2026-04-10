2dbd3be16193df94b2975c15b17f0955a4f92e3e
cesarom18 <cesarosses885@gmail.com>
2026-04-10 16:08:33 -0400

    Change CORS allowed methods

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.frontend_domain],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 # Add routers
