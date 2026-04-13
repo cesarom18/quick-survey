12b590c60248543cf9dcec381b1b532218bce4cf
cesarom18 <cesarosses885@gmail.com>
2026-04-13 19:19:07 -0400

    Implement register user endpoint into Register page form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -5,46 +5,78 @@ import {
 	Link as HeroLink,
 	Input,
 	Label,
+	Spinner,
 	TextField,
+	toast,
 } from "@heroui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import type { z } from "zod";
-
 import { registerUser } from "../features/auth/schemas.ts";
+import type { User } from "../features/auth/types.ts";
+import { ENDPOINTS } from "../shared/constanst.ts";
+import { useFetch } from "../shared/hooks/useFetch.ts";
 
 type FormValues = z.infer<typeof registerUser>;
 
 export const Register = () => {
+	const navigate = useNavigate();
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm<FormValues>({ resolver: zodResolver(registerUser) });
+	} = useForm<FormValues>({
+		resolver: zodResolver(registerUser),
+	});
+	const { loading, execute } = useFetch<User>();
 
-	const onSubmit = (data: FormValues) => {
-		console.log(data);
+	const onSubmit = async (data: FormValues) => {
+		try {
+			await execute(ENDPOINTS.AUTH.REGISTER, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(data),
+			});
+			toast.success("Successfully registered user");
+            navigate("/auth/login");
+		} catch (err) {
+			toast.danger("An error occurred while registering user");
+		}
 	};
 
 	return (
 		<Form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-			<TextField type="text" name="name" isInvalid={!!errors.name}>
+			<TextField
+				type="text"
+				name="name"
+				isInvalid={!!errors.name}
+				isDisabled={loading}
+			>
 				<Label>Name</Label>
 				<Input variant="secondary" placeholder="John" {...register("name")} />
 				{errors.name && <FieldError>{errors.name.message}</FieldError>}
 			</TextField>
-			<TextField type="email" name="email" isInvalid={!!errors.email}>
+			<TextField
+				type="email"
+				name="email"
+				isInvalid={!!errors.email}
+				isDisabled={loading}
+			>
 				<Label>Email</Label>
 				<Input
 					variant="secondary"
 					placeholder="john@example.com"
 					{...register("email")}
 				/>
-
 				{errors.email && <FieldError>{errors.email.message}</FieldError>}
 			</TextField>
-			<TextField type="password" name="password" isInvalid={!!errors.password}>
+			<TextField
+				type="password"
+				name="password"
+				isInvalid={!!errors.password}
+				isDisabled={loading}
+			>
 				<Label>Password</Label>
 				<Input
 					variant="secondary"
@@ -57,6 +89,7 @@ export const Register = () => {
 				type="password"
 				name="confirm_password"
 				isInvalid={!!errors.confirm_password}
+				isDisabled={loading}
 			>
 				<Label>Confirm Password</Label>
 				<Input
@@ -68,8 +101,20 @@ export const Register = () => {
 					<FieldError>{errors.confirm_password.message}</FieldError>
 				)}
 			</TextField>
-			<Button className="text-white rounded-xl" type="submit" size="lg" fullWidth>
-				Sign Up
+			<Button
+				className="text-white rounded-xl"
+				type="submit"
+				size="lg"
+				isPending={loading}
+				fullWidth
+			>
+				{loading ? (
+					<>
+						Loading <Spinner color="current" />
+					</>
+				) : (
+					<>Sign Up</>
+				)}
 			</Button>
 			<div className="text-center">
 				<HeroLink className="no-underline hover:underline">
---
+++ frontend/src/shared/constanst.ts
@@ -0,0 +1,7 @@
+export const API_URL = import.meta.env.VITE_API_URL;
+
+export const ENDPOINTS = {
+	AUTH: {
+		REGISTER: `${API_URL}/auth/register`,
+	},
+};
