b3c75001fea6c3b21229feb4fc238712c068360c
cesarom18 <cesarosses885@gmail.com>
2026-04-15 17:15:58 -0400

    Fix revert problems

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,96 +1,40 @@
 import {
 	Button,
-	FieldError,
 	Form,
 	Link as HeroLink,
 	Input,
 	Label,
-	Spinner,
 	TextField,
-	toast,
 } from "@heroui/react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router";
+import { Link } from "react-router";
 import type { z } from "zod";
-import { loginUser } from "../features/auth/schemas";
+import type { loginUser } from "../features/auth/schemas";
 import { ENDPOINTS } from "../shared/constanst";
-import { useFetch } from "../shared/hooks/useFetch";
 
-type FormValues = z.infer<typeof loginUser>;
 
 export const Login = () => {
-	const navigate = useNavigate();
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<FormValues>({ resolver: zodResolver(loginUser) });
-	const { loading, execute } = useFetch();
-
-	const onSubmit = async (data: FormValues) => {
-		try {
-			await execute(ENDPOINTS.AUTH.LOGIN, {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-				body: JSON.stringify(data),
-			});
-			toast.success("Successfully login user");
-			navigate("/auth/login");
-		} catch (err) {
-			toast.danger("An error occurred while login user");
-		}
-	};
-
 	return (
-		<Form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-			<TextField
-				type="email"
-				name="email"
-				isInvalid={!!errors.email}
-				isDisabled={loading}
-			>
+		<Form className="flex flex-col gap-4" >
+			<TextField type="email" name="email">
 				<Label>Email</Label>
-				<Input
-					variant="secondary"
-					placeholder="john@example.com"
-					{...register("email")}
-				/>
-				{errors?.email && <FieldError>{errors.email.message}</FieldError>}
+				<Input variant="secondary" placeholder="john@example.com" />
 			</TextField>
-			<TextField
-				type="password"
-				name="password"
-				isInvalid={!!errors.password}
-				isDisabled={loading}
-			>
+			<TextField type="password" name="password">
 				<div className="flex justify-between">
 					<Label>Password</Label>
 					<HeroLink className="no-underline hover:underline">
 						<Link to="">Forgot?</Link>
 					</HeroLink>
 				</div>
-				<Input
-					variant="secondary"
-					placeholder="Enter your password"
-					{...register("password")}
-				/>
-				{errors?.password && <FieldError>{errors.password.message}</FieldError>}
+				<Input variant="secondary" placeholder="Enter your password" />
 			</TextField>
 			<Button
 				className="text-white rounded-xl"
 				type="submit"
 				size="lg"
-				isPending={loading}
 				fullWidth
 			>
-				{loading ? (
-					<>
-						Loading <Spinner color="current" />
-					</>
-				) : (
-					<>Sign In</>
-				)}
+				Sign In
 			</Button>
 			<div className="text-center">
 				<HeroLink className="no-underline hover:underline">
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,120 +1,43 @@
 import {
 	Button,
-	FieldError,
 	Form,
 	Link as HeroLink,
 	Input,
 	Label,
-	Spinner,
 	TextField,
-	toast,
 } from "@heroui/react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router";
+import { Link } from "react-router";
 import type { z } from "zod";
 import { registerUser } from "../features/auth/schemas.ts";
 import type { User } from "../features/auth/types.ts";
 import { ENDPOINTS } from "../shared/constanst.ts";
-import { useFetch } from "../shared/hooks/useFetch.ts";
-
-type FormValues = z.infer<typeof registerUser>;
 
 export const Register = () => {
-	const navigate = useNavigate();
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<FormValues>({
-		resolver: zodResolver(registerUser),
-	});
-	const { loading, execute } = useFetch<User>();
-
-	const onSubmit = async (data: FormValues) => {
-		try {
-			await execute(ENDPOINTS.AUTH.REGISTER, {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify(data),
-			});
-			toast.success("Successfully registered user");
-            navigate("/auth/login");
-		} catch (err) {
-			toast.danger("An error occurred while registering user");
-		}
-	};
-
 	return (
-		<Form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-			<TextField
-				type="text"
-				name="name"
-				isInvalid={!!errors.name}
-				isDisabled={loading}
-			>
+		<Form className="flex flex-col gap-4">
+			<TextField type="text" name="name">
 				<Label>Name</Label>
-				<Input variant="secondary" placeholder="John" {...register("name")} />
-				{errors.name && <FieldError>{errors.name.message}</FieldError>}
+				<Input variant="secondary" placeholder="John" />
 			</TextField>
-			<TextField
-				type="email"
-				name="email"
-				isInvalid={!!errors.email}
-				isDisabled={loading}
-			>
+			<TextField type="email" name="email">
 				<Label>Email</Label>
-				<Input
-					variant="secondary"
-					placeholder="john@example.com"
-					{...register("email")}
-				/>
-				{errors.email && <FieldError>{errors.email.message}</FieldError>}
+				<Input variant="secondary" placeholder="john@example.com" />
 			</TextField>
-			<TextField
-				type="password"
-				name="password"
-				isInvalid={!!errors.password}
-				isDisabled={loading}
-			>
+			<TextField type="password" name="password">
 				<Label>Password</Label>
-				<Input
-					variant="secondary"
-					placeholder="Enter your password"
-					{...register("password")}
-				/>
-				{errors.password && <FieldError>{errors.password.message}</FieldError>}
+				<Input variant="secondary" placeholder="Enter your password" />
 			</TextField>
-			<TextField
-				type="password"
-				name="confirm_password"
-				isInvalid={!!errors.confirm_password}
-				isDisabled={loading}
-			>
+			<TextField type="password" name="confirm_password">
 				<Label>Confirm Password</Label>
-				<Input
-					variant="secondary"
-					placeholder="Enter your password"
-					{...register("confirm_password")}
-				/>
-				{errors.confirm_password && (
-					<FieldError>{errors.confirm_password.message}</FieldError>
-				)}
+				<Input variant="secondary" placeholder="Enter your password" />
 			</TextField>
 			<Button
 				className="text-white rounded-xl"
 				type="submit"
 				size="lg"
-				isPending={loading}
 				fullWidth
 			>
-				{loading ? (
-					<>
-						Loading <Spinner color="current" />
-					</>
-				) : (
-					<>Sign Up</>
-				)}
+				Sign Up
 			</Button>
 			<div className="text-center">
 				<HeroLink className="no-underline hover:underline">
