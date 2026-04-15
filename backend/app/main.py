3235e7619f4859ee0387059d88af13f100b9d396
cesarom18 <cesarosses885@gmail.com>
2026-04-15 17:07:58 -0400

    Revert "Change CORS allowed methods"
    This reverts commit 2dbd3be16193df94b2975c15b17f0955a4f92e3e.

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.frontend_domain],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 # Add routers
